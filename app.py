325b7221763b63650fd0d8e06553405a6981fd49
Aleksander <burnat.a10@gmail.com>
2019-12-04 07:51:32 +0100

    reevaluated version 0.1 not tested

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 from cassandra.cluster import Cluster
+import cassandra
 
-cluster = Cluster()
+
+cluster = Cluster('casstest://localhost:2375')
+
+
+class message():
+    def post(self):
+        session = cluster.connect('node')
+        rows = session.execute("SELECT content")
+        return rows
+
+class write():
+    def post(self):
+        session = cluster.connect('node')
+        rows = session.execute("")
+        return rows
+
+class emailValue():
+    def get(self):
+        session = cluster.connect('node')
+        rows = session.execute("SELECT email")
+        return rows
+class reset():
+    def timer():
+        t =
