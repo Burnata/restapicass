912bf4ee26ea835d32372ed86f99610fdb32d809
Aleksander <burnat.a10@gmail.com>
2019-12-05 23:31:20 +0100

    version 0.2 not tested

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import re
-import time
 
 from cassandra.cluster import Cluster
 from cassandra.query import tuple_factory
 
 
-cluster = Cluster(["127.0.0.1"])
+cluster = Cluster(["0.0.0.0"])
 regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
 
 def post(self):
-    session = cluster.connect(["127.0.0.1"])
+    session = cluster.connect(["0.0.0.0"])
     session.row_factory = tuple_factory
     rows = session.execute("SELECT *")
     return rows
@@ -18,7 +17,7 @@ def post(self):
 
 def post(email, title, content, magic):
     if re.search(regex, email):
-        session = cluster.connect(["127.0.0.1"])
+        session = cluster.connect(["0.0.0.0"])
         session.execute("INSERT INTO node (email,title,content,magic_number) VALUES (%s,%s,%s,%s)",
                         (email, title, content, magic))
     else:
@@ -26,7 +25,7 @@ def post(email, title, content, magic):
 
 
 def get(self):
-    session = cluster.connect(["127.0.0.1"])
+    session = cluster.connect(["0.0.0.0"])
     session.row_factory = tuple_factory
     rows = session.execute("SELECT email")
     return rows
