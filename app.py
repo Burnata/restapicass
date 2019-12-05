834c63fdc4492837f3b5f13e7fa79ea10487b24f
Aleksander <burnat.a10@gmail.com>
2019-12-05 12:11:22 +0100

    reevaluated version 0.1 not tested

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 from cassandra.cluster import Cluster
 import cassandra
+import time
+import re
+
+cluster = Cluster('casstest://127.0.0.1:9160')
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
 
-cluster = Cluster('casstest://localhost:2375')
+def post(self):
+    session = cluster.connect('node')
+    rows = session.execute("SELECT content")
+    return rows
+
+def post(email,title,content,magic):
+    if (re.search(regex, email)):
+        print()
+    else:
+        print("Invalid Email")
+    session = cluster.connect('node')
+    session.execute("INSERT INTO node (email,title,content,magic_number) "
+                               "VALUES(email,title,content,magic)")
+
+def get(self):
+    session = cluster.connect('node')
+    rows = session.execute("SELECT email")
+    return rows
 
 
-class message():
-    def post(self):
-        session = cluster.connect('node')
-        rows = session.execute("SELECT content")
-        return rows
-
-class write():
-    def post(self):
-        session = cluster.connect('node')
-        rows = session.execute("")
-        return rows
-
-class emailValue():
-    def get(self):
-        session = cluster.connect('node')
-        rows = session.execute("SELECT email")
-        return rows
-class reset():
-    def timer():
-        t =
+def timer():
+    session = cluster.connect('node')
+    t0 = time.clock()
+    x = 1
+    if time.clock() / 300 * x == 0:
+        session.execute('TRUNCATE node')
+        x = x + 1
