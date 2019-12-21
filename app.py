2567f5c8e0b7a62d8e76bc0391cd5c56a8e28d55
Aleksander <burnat.a10@gmail.com>
2019-12-21 23:05:40 +0100

    version 0.8

--- app.py
+++ app.py
@@ -19,21 +19,26 @@ def post():
     data = json.dumps(data)
     datain = int(re.search(r'\d+', data).group())
     raw = session.prepare('SELECT * FROM test.mode WHERE magic_number=? ALLOW FILTERING')
-    session.execute(raw, [datain])
-    de = session.prepare('DELETE FROM test.mode WHERE magic_number IN ?')
-    session.execute(de, [datain])
-    return jsonify(list(raw))
+    res = session.execute(raw, [datain])
+    count = session.prepare('SELECT COUNT * FROM test.mode WHERE magic_number=? ALLOW FILTERING')
+    a = session.execute(count, [datain])
+    ral = session.prepare('SELECT content FROM test.mode WHERE magic_number=? ALLOW FILTERING')
+    rek = session.execute(ral, [datain])
+    while a > 0:
+        race = json.dumps(list(rek)[a])
+        dede = session.prepare("UPDATE test.mode USING TTL 1 SET title = 'x' WHERE content=?")
+        session.execute(dede, [race])
+        a = a-1
+    return jsonify(list(res))
 
 
 @app.route('/api/message', methods=['POST'])
 def posted():
     data = request.get_json(force=True)
     data = json.dumps(data)
-    insert = session.prepare('INSERT INTO test.mode JSON ?')
+    insert = session.prepare('INSERT INTO test.mode JSON ? USING TTL 300')
     session.execute(insert, [data])
     pop = session.execute("SELECT * FROM test.mode")
-    timer = time.clock()
-
     return jsonify(list(pop))
 
 
